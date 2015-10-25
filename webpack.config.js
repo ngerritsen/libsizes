4bf9c46b8d430161b8e425336bb7d9c8d5a33dbf
Niels Gerritsen <n.gerritsen@coolblue.nl>
2015-10-25 11:47:26 +0100

    switched to production

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
