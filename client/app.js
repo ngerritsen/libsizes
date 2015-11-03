82f85fdfbdbbe3b04e26efd6bd6df0b4c916f0ff
Niels Gerritsen <n.c.gerritsen@gmail.com>
2015-11-03 14:54:36 +0100

    added github link;

--- client/app.js
+++ client/app.js
@@ -15,6 +15,9 @@ export default class App extends Component {
           <p className="note">
             *Sizes may vary according to bundler, minifier and their settings.
             Here, minified sizes are generated using Webpack with the UglifyJS plugin.</p>
+          <a className="footer--github-link" href="https://github.com/ngerritsen/libsizes" target="_blank" title="Check out the Github repository">
+            <span className="mega-octicon octicon-mark-github"></span> View on Github
+          </a>
           <p className="footer-text">{String.fromCharCode(169)} Niels Gerritsen 2015</p>
         </div>
       </div>
--- client/header.js
+++ client/header.js
@@ -7,7 +7,14 @@ export default () => (
   <div className="header">
     <div className="container-fluid">
       <h1 className="header--title">libsizes</h1>
-      <p className="header--quote">{libraries.length} library sizes generated from package.json</p>
+      <p className="header--quote">
+        {libraries.length} library sizes generated from package.json
+        <span className="seperator"></span>
+        <a className="header--github-link" href="https://github.com/ngerritsen/libsizes" target="_blank" title="Check out the Github repository">
+          <span className="octicon octicon-mark-github"></span> View on Github
+        </a>
+      </p>
+
     </div>
   </div>
 )
