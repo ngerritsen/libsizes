b75124af86206efd1c3744d550a6d6938364a6ea
Niels Gerritsen <n.c.gerritsen@gmail.com>
2016-12-05 11:30:48 +0100

    Add skipped state

--- client/components/analysis.js
+++ client/components/analysis.js
@@ -8,25 +8,31 @@ const statusClassMap = {
   [constants.ANALYSIS_STATUS_WAITING]: 'waiting',
   [constants.ANALYSIS_STATUS_PENDING]: 'pending',
   [constants.ANALYSIS_STATUS_SUCCEEDED]: 'succeeded',
+  [constants.ANALYSIS_STATUS_SKIPPED]: 'skipped',
   [constants.ANALYSIS_STATUS_FAILED]: 'failed'
+};
+
+const messageClassMap = {
+  [constants.ANALYSIS_STATUS_FAILED]: 'error',
+  [constants.ANALYSIS_STATUS_SUCCEEDED]: 'success',
+  [constants.ANALYSIS_STATUS_SKIPPED]: 'skipped'
 };
 
 const statusIconMap = {
   [constants.ANALYSIS_STATUS_WAITING]: 'circle-o-notch',
   [constants.ANALYSIS_STATUS_PENDING]: 'circle-o-notch fa-spin',
   [constants.ANALYSIS_STATUS_SUCCEEDED]: 'check',
+  [constants.ANALYSIS_STATUS_SKIPPED]: 'check',
   [constants.ANALYSIS_STATUS_FAILED]: 'times'
 };
 
 function Analysis({ id, error, message, result, status, libraryString }) {
   const messageToShow = error || resultToMessage(result) || message;
-  const messageClassName = 'analysis__message' +
-    (error ? ' analysis__message--error' : '') +
-    (result ? ' analysis__message--success' : '');
+  const messageTypeClassName = messageClassMap[status] ? ' analysis__message--' + messageClassMap[status] : '';
 
   return (
     <li key={id} className="analysis">
-      <span className={messageClassName}>{messageToShow}</span>
+      <span className={'analysis__message' + messageTypeClassName}>{messageToShow}</span>
       <i className={`analysis__status analysis__status--${statusClassMap[status]}`}/>
       <span className="analysis__status-icon-container">
         <i className={`
